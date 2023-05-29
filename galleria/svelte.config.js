70d592e5f8ea6cee58ad17df1490c574ec6c9fd7
Cristiano Ledur <cristianoledur98@gmail.com>
2023-05-29 00:42:07 -0300

    Ajustando svelte config

--- galleria/svelte.config.js
+++ galleria/svelte.config.js
@@ -1,11 +1,20 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
+import { resolve } from 'path';
  
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-		runtime: 'edge',
-    }),
+    // adapter: adapter(),
+	adapter: vercel(),
+	vite: {
+		resolve: {
+			alias: {
+				$src: resolve('./src'),
+				$components: resolve('./src/components'),
+				$sections: resolve('./src/sections')
+			}
+		}
+	}
   },
 };
  
